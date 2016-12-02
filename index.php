cc555eec003496cf045a4bc68252ff322e5e6ed4
Azucena Rosales <azucena.rosales@gmail.com>
2016-12-02 11:55:51 -0600

    header y navbar php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include_once('header.php');
+      include_once('navbar.php'); ?>      
+
+  <img  height="50%" width="50%" src="img/colegio_del_bosque_logo.jpg" alt="Colegio del Bosque" weight="20%">
+
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,27 @@
+  <!-- Dropdown Structure -->
+<ul id="dropdown1" class="dropdown-content">
+  <li><a href="#!">Silabas</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">Agudas</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">Graves</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">Esdrujulas</a></li>
+</ul>
+
+  <nav>
+    <div class="nav-wrapper">
+      <img  height="70px" width="70px" class="brand-logo left" src="img/colegio_del_bosque_logo.jpg" alt="Colegio del Bosque" weight="20%">
+      
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <!-- Dropdown Trigger -->
+        <li><a class="dropdown-button" href="#!" data-activates="dropdown1" id="resumen">Resumen<i class="material-icons right">arrow_drop_down</i></a></li>
+        <li><a href="ejercicios/">Ejercicios</a></li>
+        <li><a href="tutoriales/">Tutoriales</a></li>
+        <li><a href="juegos/">Juegos</a></li>
+        <li><a href="manual/">Manual</a></li>
+        
+
+      </ul>
+    </div>
+  </nav>
